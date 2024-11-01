b6ddc74dc97944e1b0595a9cad4ee86bd39c824a
Goyabean <118635203+GeObts@users.noreply.github.com>
2024-11-01 14:14:52 -0600

    Update OnchainStoreProvider.tsx

--- src/components/OnchainStoreProvider.tsx
+++ src/components/OnchainStoreProvider.tsx
@@ -1,7 +1,6 @@
 import { createContext, useContext, useMemo, useState } from 'react';
 import type { ReactNode } from 'react';
 import type { OnchainStoreContextType } from '../types';
-import jacketImage from '../images/jacket.png';
 import airpodsImage from '../images/airpods.png';
 import mugImage from '../images/mug.png';
 import bottleImage from '../images/bottle.png';
@@ -9,47 +8,37 @@ import type { Product } from 'src/types';
 
 const emptyContext = {} as OnchainStoreContextType;
 
-const OnchainStoreContext =
-  createContext<OnchainStoreContextType>(emptyContext);
+const OnchainStoreContext = createContext<OnchainStoreContextType>(emptyContext);
 
 type OnchainStoreProviderReact = {
   children: ReactNode;
 };
 
 const products: Product[] = [
-
-
   { 
     id: 'fb56e47f-d7da-4b74-a0b3-d0e197d47f80', 
     name: `'BASED' SUPERFOOD`, 
     price: 29.99, 
-    image: 'https://res.cloudinary.com/commerce/image/upload/v1719508080/pcgpypu8zmp6wirxsybz.jpg'  // Ensure this ends with a quote, not a semicolon
+    image: 'https://res.cloudinary.com/commerce/image/upload/v1719508080/pcgpypu8zmp6wirxsybz.jpg'
   },
   {
     id: 'product2',
     name: `'DND, I'M BUILDING' AIRPODS`,
     price: 0.01,
-    image: airpodsImage  // Make sure airpodsImage is properly imported
-  }
-];
-
-    id: 'product2',
-    name: `'DND, I'M BUILDING' AIRPODS`,
-    price: 0.01,
-    image: airpodsImage,
+    image: airpodsImage
   },
   {
     id: 'product3',
     name: `'CAFFEINATED TO BUILD' MUG`,
     price: 0.02,
-    image: mugImage,
+    image: mugImage
   },
   {
     id: 'product4',
     name: `'HYDRATED TO BUILD' BOTTLE`,
     price: 0.01,
-    image: bottleImage,
-  },
+    image: bottleImage
+  }
 ];
 
 export function OnchainStoreProvider({ children }: OnchainStoreProviderReact) {
